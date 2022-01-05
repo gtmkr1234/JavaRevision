a1e2c250770a07ceb9303b84da79ead3947e6130
pc <gtmkr1234@gmail.com>
2022-01-05 09:30:48 +0530

    init commit

---
+++ src/anagramMethod.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class anagramMethod {
+    public void anagram(String a, String b){
+        char []c = a.toCharArray();
+        char []d = b.toCharArray();
+        int flag =0;
+        if (a.length()==b.length()){
+            for(int i=0; i<a.length();i++){
+                for(int j=0 ; j<a.length();j++){
+                    if(c[i]==d[j]){
+                        flag++;
+                    }
+                }
+            }
+            if(flag==a.length()){
+                System.out.println("Strings are anagram");
+            }
+            else{
+                System.out.println("Strings are not anagram");
+            }
+        }
+        else{
+            System.out.println("Strings are not anagram");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
