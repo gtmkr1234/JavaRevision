8b731a9e806a226283682edefbbe9104afbdc821
pc <gtmkr1234@gmail.com>
2022-01-06 22:19:10 +0530

    init commit

---
+++ src/book.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class book {
+    int bookid;
+    String bookname;
+    int pages;
+    int price;
+    book(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the book id ");
+        bookid = s.nextInt();
+        System.out.println("Enter the name of the book");
+        bookname = s.next();
+        System.out.println("Enter the no. of pagers of the book");
+        pages = s.nextInt();
+        System.out.println("Enter the price of the book");
+        price = s.nextInt();
+    }
+
+    public void showDetails(){
+        System.out.println("Book id is "+ bookid);
+        System.out.println("Bookname is " + bookname);
+        System.out.println("The book has " + pages + "number of pages");
+        System.out.println("the price of the book is " + price);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
