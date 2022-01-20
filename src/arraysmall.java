e46753f53c906bd5eea90307ed0d3a57a1578ac3
pc <gtmkr1234@gmail.com>
2022-01-20 23:14:33 +0530

    find nth smallest element in an array

--- src/arraysmall.java
+++ src/arraysmall.java
@@ -1,17 +1,36 @@
+
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class arraysmall {
     int n;
-    int [] arr = new int [n];
-    public void createArray(){
+    int [] arr ;
+    arraysmall(){
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the size of array: ");
         n = s.nextInt();
+        arr = new int[n];
         System.out.println("Enter "+ n + " elements of the array:");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = s.nextInt();
         }
     }
+public int[] sortArray(){
+       Arrays.sort(arr);
+       return arr;
+}
+    public void printElement(int [] arr, int k){
+        System.out.println(arr[k-1]);
+    }
+}
 
-
+class Exe{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        arraysmall obj = new arraysmall();
+        System.out.println("Enter the value of k :");
+        int k = s.nextInt();
+        obj.sortArray();
+        obj.printElement(obj.arr,k);
+    }
 }
