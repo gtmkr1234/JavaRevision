d9cf453fe7a449b39bb514b2f59e2ab90b586048
pc <gtmkr1234@gmail.com>
2022-01-21 23:13:32 +0530

    find the element in an array recursively

---
+++ src/RecursiveIndex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RecursiveIndex {
+    int n;
+    int arr[];
+    RecursiveIndex(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of elements :");
+        n = s.nextInt();
+        arr = new int[n];
+        System.out.println("Enter the elements");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.nextInt();
+        }
+        s.close();
+    }
+
+
+}
