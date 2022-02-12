b83187c8615ec856bbcef881d99938562e3f8b34
pc <gtmkr1234@gmail.com>
2022-02-13 00:02:34 +0530

    find the element in an array recursively

---
+++ src/dsa/LinkedList.java
@@ -0,0 +1,13 @@
+package dsa;
+
+public class LinkedList {
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
